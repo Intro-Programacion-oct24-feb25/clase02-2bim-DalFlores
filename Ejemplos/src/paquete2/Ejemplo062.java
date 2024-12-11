d17c107343ff5599755fe4612281f2fbf1689c90
Dalton Flores <daltonflores@gmail.com>
2024-12-11 12:38:40 -0500

    Cambios#

--- Ejemplos/src/paquete2/Ejemplo062.java
+++ Ejemplos/src/paquete2/Ejemplo062.java
@@ -1,10 +1,13 @@
-* To change this license header, choose License Headers in Project Properties.
+/*
+ * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package paquete2;
+
 import java.util.Scanner;
 import java.util.Locale;
+
 /**
  *
  * @author reroes
